e4406a32b104e3a9101f34f4d420bd8db3dce729
Michael Anderson <michanders@msn.com>
2018-03-01 01:38:19 +0000

    automatically backed up by learn

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -19,7 +19,7 @@ class PoemWriter extends React.Component {
     var valid = false;
     var poem = this.state.value.split(". ");
     var line = peom.map(line => line.split(" "));
-  
+
     if (poem.length === 3 && line[0].length === 5 && line[1] === 3 && line[2] === 5){
       let valid = true;
     }
@@ -30,9 +30,9 @@ class PoemWriter extends React.Component {
     return (
       <div>
         <textarea rows="3" cols="60" value={this.state.value} onChange={this.poemChange}/>
-        <div id="poem-validation-error" style={{ color: "red" }}>
+        {checkPoem ? <div id="poem-validation-error" style={{ color: "red" }}>
           This poem is not written in the right structure!
-        </div>
+        </div> : null}
       </div>
     );
   }
