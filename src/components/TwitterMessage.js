3979f2ec1974072e8986cfaabe6d18ff0bb7453b
Michael Anderson <michanders@msn.com>
2018-03-01 00:58:50 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -19,7 +19,9 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text"
+        value = {this.handleChange} 
+        />
       </div>
     );
   }
